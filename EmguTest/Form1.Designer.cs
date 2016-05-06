00303a4d259ca448bf4e83119028058076136e27
Shuji Urano <shuji.urano.fg@gmail.com>
2016-05-06 22:00:12 +0900

    Add files

---
+++ EmguTest/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace EmguTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.ocrTextBox = new System.Windows.Forms.TextBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadLanguageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(621, 25);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 0;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.imageBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.ocrTextBox);
+            this.splitContainer2.Size = new System.Drawing.Size(853, 561);
+            this.splitContainer2.SplitterDistance = 426;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // ocrTextBox
+            // 
+            this.ocrTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ocrTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ocrTextBox.Location = new System.Drawing.Point(0, 0);
+            this.ocrTextBox.Multiline = true;
+            this.ocrTextBox.Name = "ocrTextBox";
+            this.ocrTextBox.ReadOnly = true;
+            this.ocrTextBox.Size = new System.Drawing.Size(423, 561);
+            this.ocrTextBox.TabIndex = 0;
+            this.ocrTextBox.WordWrap = false;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox1.Location = new System.Drawing.Point(0, 0);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(426, 561);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(853, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadLanguageToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadLanguageToolStripMenuItem
+            // 
+            this.loadLanguageToolStripMenuItem.Name = "loadLanguageToolStripMenuItem";
+            this.loadLanguageToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.loadLanguageToolStripMenuItem.Text = "Load Language";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.buttonLoad);
+            this.splitContainer1.Panel1.Controls.Add(this.fileNameTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.menuStrip1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(853, 626);
+            this.splitContainer1.SplitterDistance = 61;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(67, 25);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.ReadOnly = true;
+            this.fileNameTextBox.Size = new System.Drawing.Size(519, 19);
+            this.fileNameTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 626);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Receipt Analyzer";
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.TextBox ocrTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadLanguageToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+    }
+}
+
---
+++ EmguTest/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Xml;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.OCR;
+using Emgu.Util;
+
+namespace EmguTest
+{
+    public partial class Form1 : Form
+    {
+        private Tesseract _ocr;
+        static NameValueCollection appSettings = ConfigurationManager.AppSettings;
+        static XmlDocument xml = new XmlDocument();
+        static Image<Gray, byte> header, footer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _ocr = new Tesseract("", "eng", OcrEngineMode.TesseractOnly);
+            string templateFile = appSettings["RECEIPT.TEMPLATE"];
+            xml.Load(templateFile);
+            XmlNode doc = xml.DocumentElement;
+
+            XmlNode node = doc.SelectSingleNode("shop/header");
+            if (node != null && node.InnerText != null)
+            {
+                string headerFile = appSettings[node.InnerText];
+                header = new Image<Gray, byte>(headerFile);
+            }
+            node = doc.SelectSingleNode("shop/footer");
+            if (node != null && node.InnerText != null)
+            {
+                string headerFile = appSettings[node.InnerText];
+                footer = new Image<Gray, byte>(headerFile);
+            }
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+          {
+              Bgr drawColor = new Bgr(Color.Blue);
+              Bgr redColor = new Bgr(Color.Red);
+              try
+              {
+                  Image<Bgr, Byte> image = new Image<Bgr, byte>(openFileDialog.FileName);
+
+                  using (Image<Hsv, byte> hsv = image.Convert<Hsv, Byte>())
+                  {
+                      int[] histogram = new int[image.Height];
+                      int[] movingAve = new int[image.Height];
+
+                      Image<Gray, byte>[] gray = hsv.Split();
+                      gray[2]._GammaCorrect(2.0);
+                      _ocr.Recognize(gray[2].Erode(1));
+
+                      // 
+                      Tesseract.Character[] characters = _ocr.GetCharacters();
+                      int height = 0;
+                      foreach (Tesseract.Character c in characters)
+                      {
+                          height += c.Region.Height;
+                          image.Draw(c.Region, drawColor, 1);
+                          for (int y = c.Region.Top; y < c.Region.Bottom; y++)
+                          {
+                              histogram[y]++;
+                          }
+                      }
+                      System.Diagnostics.Debug.WriteLine(height / characters.Length);
+                      height /= characters.Length;
+                      
+                      // Draw histogram
+                      LineSegment2D line = new LineSegment2D(new Point(histogram[0], 0), new Point(histogram[1], 1));
+                      for (int y = 1; y < image.Height - 1; y++)
+                      {
+                          image.Draw(line, drawColor, 1);
+                          line.P1 = line.P2;
+                          line.P2 = new Point(histogram[y + 1], y + 1);
+                      }
+                      
+                      /*
+                      // Draw Moving Average
+                      line.P1 = new Point(0, 5);
+                      for (int y = 5; y < image.Height - 5; y++)
+                      {
+                          int sum = histogram[y - 5] + histogram[y - 4] + histogram[y - 3] + histogram[y -2] + histogram[y - 1];
+                          sum += histogram[y] + histogram[y + 1] + histogram[y + 2] + histogram[y + 3] + histogram[y + 4];
+                          line.P2 = new Point(sum, y);
+                          image.Draw(line, drawColor, 2);
+                          line.P1 = line.P2;
+                      }
+                      */
+
+                      bool zero = false;
+                      int zeroBegin = 0;
+                      line.P1 = new Point(0, height / 2);
+                      for (int y = height / 2; y < image.Height - height / 2; y++ )
+                      {
+                          int sum = histogram[y - height / 2] + histogram[y - height / 4] + histogram[y] + histogram[y + height / 4] + histogram[y + height / 2];
+                          line.P2 = new Point(sum, y);
+                          image.Draw(line, redColor, 1);
+                          line.P1 = line.P2;
+                          if (line.P2.X <= 3)
+                          {
+                              if (!zero)
+                              {
+                                  zero = true;
+                                  zeroBegin = y;
+                              }
+                          }
+                          else if (zero)
+                          {
+                              zero = false;
+                              LineSegment2D separator = new LineSegment2D(new Point(0, (zeroBegin + y) / 2), new Point(image.Width, (zeroBegin + y) / 2));
+                              image.Draw(separator, redColor, 3);
+                          }
+                      }
+                          
+                      imageBox1.Image = image;
+
+                      String text = _ocr.GetText();
+                      ocrTextBox.Text = text;
+                      image.Save("result.png");
+                  }
+              }
+              catch (Exception exception)
+              {
+                  MessageBox.Show(exception.Message);
+              }
+          }
+        }
+    }
+}
